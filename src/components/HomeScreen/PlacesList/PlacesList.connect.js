b7e1f000eca686d20eba58aab323a3b41d5ef5c9
BabylonBwoy <junglecrew@gmail.com>
2018-04-20 04:17:28 +0200

    sort points by range

--- src/components/HomeScreen/PlacesList/PlacesList.connect.js
+++ src/components/HomeScreen/PlacesList/PlacesList.connect.js
@@ -6,17 +6,17 @@ import { getUserPosition } from 'reducers/location/actions'
 import { toggleFilterWindow } from 'reducers/filter/actions'
 import { clearSearchForm, searchClear } from 'reducers/search/actions'
 
-import { getContentPoints, getIsFetching, getFilteredPoints } from 'reducers/content/selectors'
+import { getIsFetching, getFilteredPoints, getPointsSortedByRange } from 'reducers/content/selectors'
 import { getFilteredCategory, showFilter } from 'reducers/filter/selectors'
 import { searchValue } from 'reducers/search/selectors'
 
 const mapStateToProps = state => {
 	return {
-		pointsList: getContentPoints(state),
 		filteredCategory: getFilteredCategory(state),
 		ifFetching: getIsFetching(state),
 		showFilter: showFilter(state),
 		searchValue: searchValue(state),
+		pointsList: getPointsSortedByRange(state),
 		filteredPoints: getFilteredPoints(state),
 	}
 }
--- src/reducers/content/selectors.js
+++ src/reducers/content/selectors.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect'
 import { getFilteredCategory } from 'reducers/filter/selectors'
+import { getUserLocation } from 'reducers/location/selectors'
 
 export const getIsFetching = state => state.content.isFetching
 export const getError = state => state.content.error
@@ -13,7 +14,32 @@ export const getIsNeedUpdate = state =>
 	getError(state) !== null ||
 	Number(new Date()) - getLastUpdate(state) > 1000 * 60 * 60 * 24
 
+export const getPointsSortedByRange = createSelector(getContentPoints, getUserLocation, (points, coords) => {
+	if (!coords) return points
+
+	const calculateDistance = (lat1, lon1, lat2, lon2) => {
+		const radlat1 = Math.PI * lat1 / 180
+		const radlat2 = Math.PI * lat2 / 180
+		const theta = lon1 - lon2
+		const radtheta = Math.PI * theta / 180
+		let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+		dist = Math.acos(dist)
+		dist = dist * 180 / Math.PI
+		dist = (dist * 60 * 1.1515 * 1.609344).toFixed(1)
+		return dist
+	}
+
+	return points.sort(
+		(a, b) =>
+			calculateDistance(coords.lat, coords.lng, a.latitude, a.longitude) -
+			calculateDistance(coords.lat, coords.lng, b.latitude, b.longitude),
+	)
+})
+
 export const getFilteredPoints = createSelector(getContentPoints, getFilteredCategory, (points, filteredCategories) => {
 	if (!points) return null
 	return points.filter(item => item.category_id.includes(filteredCategories))
 })
+
+//coords.lat
+//coords.lng
